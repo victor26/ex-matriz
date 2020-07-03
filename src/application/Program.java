9c6b00807d4feb7a23b061ae5dbdb7d2f9bb90cf
victor26 <victor-sousa1996@hotmail.com>
2020-07-03 15:13:32 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		sc.nextLine();
+		int c = sc.nextInt();
+		
+		int [][] mat = new int [l][c];
+		for(int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+				}
+			}
+		
+		System.out.println("Please insert number of you wish");
+		int pos = sc.nextInt();
+		for(int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat[i].length; j++) {
+					if(mat[i][j] == pos) {
+						System.out.println("Position is :"+ i +" , "+ j);
+							if (i > 0) {
+								System.out.println("Up:"+ mat[i - 1][j]);
+							}
+							if (j > 0) {
+								System.out.println("left:"+ mat[i][j - 1]);
+							}
+							if (j < mat[i].length-1) {
+								System.out.println("Right:"+ mat[i][j +1 ]);
+							}
+							if (i < mat.length-1) {
+								System.out.println("Down:"+ mat[i + 1][j]);
+							}
+						}
+						
+					  
+					
+				}
+			}
+		
+		
+		sc.close();
+
+	}
+
+}
